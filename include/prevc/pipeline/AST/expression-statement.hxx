a8414d99a9a09ee7d4fa6686ec0fec3978fc4046
Janez Pintar <pintarj.zero@gmail.com>
2018-05-31 11:27:03 +0200

    added the 'expression statement' AST node

---
+++ include/prevc/pipeline/AST/expression-statement.hxx
@@ -0,0 +1,56 @@
+
+#ifndef PREVC_PIPELINE_AST_EXPRESSIONSTATEMENT_HXX
+#define PREVC_PIPELINE_AST_EXPRESSIONSTATEMENT_HXX
+
+#include <prevc/pipeline/AST/expression.hxx>
+#include <prevc/pipeline/AST/statement.hxx>
+
+namespace prevc
+{
+    namespace pipeline
+    {
+        namespace AST
+        {
+            /**
+             * \brief An expression statement in the AST.
+             * */
+            class ExpressionStatement: public Statement
+            {
+            public:
+
+                /**
+                 * \brief Create an expression statement node.
+                 * \param pipeline The pipeline that owns this AST node.
+                 * \param location The location of the atom in the source code.
+                 * \param expression The contained expression.
+                 * */
+                ExpressionStatement(Pipeline* pipeline, util::Location&& location, Expression* expression);
+
+                /**
+                 * \brief Release the used resources.
+                 * */
+                virtual ~ExpressionStatement();
+
+                /**
+                 * \brief Generate the IR code for this expression statement expression.
+                 * \param builder The builder of the IR block containing this expression statement.
+                 * */
+                virtual void generate_IR(llvm::IRBuilder<>* builder) override;
+
+                /**
+                 * \brief Returns a string representation of this expression statement.
+                 * \return The representation in JSON format.
+                 * */
+                virtual util::String to_string() const noexcept override;
+
+            private:
+                /**
+                 * \brief The contained expression.
+                 * */
+                 Expression* expression;
+            };
+        }
+    }
+}
+
+#endif
---
+++ src/pipeline/AST/expression-statement.cxx
@@ -0,0 +1,36 @@
+#include <prevc/pipeline/AST/expression-statement.hxx>
+#include <utility>
+
+namespace prevc
+{
+    namespace pipeline
+    {
+        namespace AST
+        {
+            ExpressionStatement::ExpressionStatement(Pipeline* pipeline, util::Location&& location, Expression* expression):
+                Statement(pipeline, std::move(location)),
+                expression(expression)
+            {
+
+            }
+
+            ExpressionStatement::~ExpressionStatement()
+            {
+                delete expression;
+            }
+
+            void ExpressionStatement::generate_IR(llvm::IRBuilder<>* builder)
+            {
+                expression->generate_IR(builder);
+            }
+
+            util::String ExpressionStatement::to_string() const noexcept
+            {
+                return util::String::format(
+                        R"({"type": "expression-statement", "location": %s, "expression": "%s"})",
+                        location.to_string().c_str(),
+                        expression->to_string().c_str());
+            }
+        }
+    }
+}
